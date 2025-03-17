cf68b4af55233b6c5a3ff83349de0f1b8df23bb5
ryanhe <casinohe@outlook.com>
2025-03-17 19:36:21 +0800

    Add formatter specialization for WTSKlinePeriod enum in fmtlib.h

--- src/Share/fmtlib.h
+++ src/Share/fmtlib.h
@@ -4,6 +4,25 @@
 #define FMT_HEADER_ONLY
 #endif
 #include <fmt/format.h>
+#include "../Includes/WTSTypes.h"
+
+// Add formatter specialization for WTSKlinePeriod enum
+template<>
+struct fmt::formatter<wtp::WTSKlinePeriod> : formatter<const char*> {
+    auto format(wtp::WTSKlinePeriod period, format_context& ctx) const {
+        const char* period_name = "";
+        switch (period) {
+            case wtp::KP_Tick:   period_name = "tick"; break;
+            case wtp::KP_Minute1: period_name = "min1"; break;
+            case wtp::KP_Minute5: period_name = "min5"; break;
+            case wtp::KP_DAY:     period_name = "day"; break;
+            case wtp::KP_Week:    period_name = "week"; break;
+            case wtp::KP_Month:   period_name = "month"; break;
+            default:             period_name = "unknown"; break;
+        }
+        return formatter<const char*>::format(period_name, ctx);
+    }
+};
 
 namespace fmtutil
 {

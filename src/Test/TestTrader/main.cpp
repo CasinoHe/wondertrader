032d4cb07d9bd21673bf78462241235a6d294192
ryanhe <casinohe@outlook.com>
2025-03-17 23:56:25 +0800

    Fix formatting issue in encoding_print function in main.cpp

--- src/Test/TestTrader/main.cpp
+++ src/Test/TestTrader/main.cpp
@@ -28,7 +28,7 @@ std::set<std::string>	g_blkList;
 template<typename... Args>
 inline void encoding_print(const char* format, const Args& ...args)
 {
-	fmt::print(FMT_RUNTIME(format), args...);
+	fmt::print(fmt::runtime(format), args...);
 	//printf(fmtutil::format(format, args...));
 //#ifdef _MSC_VER
 //	printf(UTF8toChar(s).c_str());
